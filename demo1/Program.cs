cace8490fe85eb099d6808e2c76fd814fc3cf2a5
Rishi <me.rishiverma@gmail.com>
2017-02-03 15:51:45 +0530

    demo1

--- demo1/Program.cs
+++ demo1/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Client;
+using Microsoft.Xrm.Client.Services;
+using Microsoft.Xrm.Sdk;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +14,40 @@ namespace demo1
     {
         static void Main(string[] args)
         {
+            CrmConnection con = new CrmConnection("CRM");
+
+            IOrganizationService service = new OrganizationService(con);
+
+            //var response = service.Execute(new WhoAmIRequest());
+
+            Entity account = new Entity("account");
+
+            account["name"] = "rishi";
+
+            account["numberofemployees"] = 12345;
+
+            account["accountcategorycode"] = new OptionSetValue(1);
+
+            account["creditlimit"] = new Money((decimal)500000);
+
+            var contact = new Entity("contact") { Id = Guid.NewGuid() };
+
+            account["primarycontactid"] = new EntityReference(contact.LogicalName, contact.Id);
+
+            account["primarycontactid"] = contact.ToEntityReference();
+
+            string name = account["name"].ToString();
+
+            if (account.Contains("name"))
+            { }
+            name = account.GetAttributeValue<string>("name");
+
+            var notfoundstring = account.GetAttributeValue<string>("notfound");
+
+            var notfoundoptionset = account.GetAttributeValue<OptionSetValue>("notfound");
+
+
+
         }
     }
 }
